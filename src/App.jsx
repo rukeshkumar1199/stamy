302bb19abf7b27bc184d7c03cd778e2f19e3c11e
rukeshkumar1199 <rukeshkumar1199@gmail.com>
2026-04-10 22:24:03 +0530

    feat: add gh pages

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import { Routes, Route } from "react-router-dom";
+
+import Footer from "./pages/Footer";
+import HeroVideo from "./pages/HeroVideo";
+import Navbar from "./pages/Navbar";
+import PhotoUpload from "./pages/PhotoUpload";
+// import OurServices from "./pages/OurServices";
+// import PortfolioCards from "./pages/PortfolioCards";
+import VideoShowcase from "./pages/VideoShowcase";
+import AboutUs from "./pages/AboutUs";
+import Enquiry from "./pages/Enquiry";
+import Body from "./pages/Body";
+import PhotoGrid from "./pages/PhotoGrid";
+import EventGallery from "./pages/EventGallery";
+import ViewEnquiries from "./pages/ViewEnquiries";
+function Home() {
+  return (
+    <>
+      <HeroVideo />
+      <VideoShowcase />
+      {/* <PortfolioCards /> */}
+      <Footer />
+    </>
+  );
+}
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/upload" element={<PhotoUpload />} />
+        <Route path="/AboutUs" element={<AboutUs />} />
+        <Route path="/Enquiry" element={<Enquiry />} />
+        <Route path="/Body" element={<Body />} />
+        <Route path="/PhotoGrid" element={<PhotoGrid />} />
+        <Route path="/EventGallery/event/:id" element={<EventGallery />} />
+        <Route path="/ViewEnquiries" element={<ViewEnquiries />} />
+      </Routes>
+    </>
+  );
+}
+
+//TODO:
+// --  add a manifest.json
+//-- lazy load images and unnneeded pages
+
+export default App;
